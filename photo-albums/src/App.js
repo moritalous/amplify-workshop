f0eb2eb013f9efd6defafe98ecbbd494c25c60e0
moritalous <moritalous@gmail.com>
2019-02-09 23:31:26 +0900

    Add Auth

--- photo-albums/src/App.js
+++ photo-albums/src/App.js
@@ -3,6 +3,12 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
+
+
 class App extends Component { 
     render() { 
         return (
@@ -13,4 +19,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
